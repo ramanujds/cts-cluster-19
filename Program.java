a094d1841a0ea62fa1ebbff07aeaf057fa0eca32
ramanujds <ramanujds9@gmail.com>
2025-11-13 14:58:04 +0530

    Updated Program.java

--- Program.java
+++ Program.java
@@ -1,5 +1,9 @@
-class Program{
+class Program {
     public static void main(String[] args) {
+
         System.out.println("Hello from CTS");
+        System.out.println("This is a simple Java program.");
+
+
     }
 }
