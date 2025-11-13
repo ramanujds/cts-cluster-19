f2638621d42dda5f7d0f3c06e6c75d1f6d70bbd8
ramanujds <ramanujds9@gmail.com>
2025-11-13 16:52:54 +0530

    deleted Program.java

--- Program.java
+++
@@ -1,9 +0,0 @@
-class Program {
-    public static void main(String[] args) {
-
-        System.out.println("Hello from CTS");
-        System.out.println("This is a simple Java program.");
-
-
-    }
-}
