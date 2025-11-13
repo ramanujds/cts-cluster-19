e19a4c756aa66ffbafdcbb83f14cd5c1597ae044
ramanujds <ramanujds9@gmail.com>
2025-11-13 16:56:31 +0530

    Revert "deleted Program.java"
    This reverts commit f2638621d42dda5f7d0f3c06e6c75d1f6d70bbd8.

---
+++ Program.java
@@ -0,0 +1,9 @@
+class Program {
+    public static void main(String[] args) {
+
+        System.out.println("Hello from CTS");
+        System.out.println("This is a simple Java program.");
+
+
+    }
+}
