4239391268abc518bf003fbb27c057e1a8777e3c
ramanujds <ramanujds9@gmail.com>
2025-11-13 15:05:06 +0530

    Added main function to App.py

--- App.py
+++ App.py
@@ -6,3 +6,8 @@ printInfo()
 def additionalFunction():
     print("Additional function in App.py")
 additionalFunction()
+
+def main():
+    print("Main function in App.py")
+if __name__ == "__main__":
+    main()
