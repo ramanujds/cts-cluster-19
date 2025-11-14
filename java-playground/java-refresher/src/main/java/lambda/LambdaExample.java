1468e6a35f1a6dfede9c45983d38d5da359b9b1d
ramanujds <ramanujds9@gmail.com>
2025-11-14 10:43:58 +0530

    Updated with lambda expression task

--- java-playground/java-refresher/src/main/java/lambda/LambdaExample.java
+++ java-playground/java-refresher/src/main/java/lambda/LambdaExample.java
@@ -1,17 +1,32 @@
 package lambda;
 
+// Functional Interface
+
+
 interface Printable {
     void print();
+}
+
+interface Calc{
+    int calculate(int a, int b);
+}
+
+interface CheckNum{
+    boolean test(int number);
 }
 
 
 public class LambdaExample {
 
     public static void main(String[] args) {
-        Printable p = () -> {
-            System.out.println("Hello");
-        };
 
-        p.print();
+        Calc add = (a, b) -> a+b;
+
+        Calc subtract = (a,b) -> a-b;
+
+        // Create lambda expression to test
+        // 1 - check if a number is even
+        // 2 - if a number is prime
+
     }
 }
